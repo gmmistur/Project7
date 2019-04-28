7a6adae951c841f0e25189993788c2a7fa4518d6
gmmistur <34195839+gmmistur@users.noreply.github.com>
2019-04-28 18:45:33 -0400

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,59 @@
+<?php 
+	include 'database.php';
+	$id = 0;
+	
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+	
+	if ( !empty($_POST)) {
+		// keep track post values
+		$id = $_POST['id'];
+		
+		// delete data
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "DELETE FROM Users WHERE id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		Database::disconnect();
+		header("Location: index.php");
+		
+	} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+	<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline'; style-src 'self' 'unsafe-inline'; media-src *" />
+				
+    <title>Project 7 - Delete</title>
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Delete a User</h3>
+		    		</div>
+		    		
+	    			<form class="form-horizontal" action="delete.php" method="post">
+	    			  <input type="hidden" name="id" value="<?php echo $id;?>"/>
+					  <p class="alert alert-error">Are you sure to delete ?</p>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-danger">Yes</button>
+						  <a class="btn" href="index.php">No</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
