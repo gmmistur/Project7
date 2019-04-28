ab9aa6879a0c0a62f7b5cd51a31d37118fb5e1c0
gmmistur <34195839+gmmistur@users.noreply.github.com>
2019-04-28 18:44:16 -0400

    Create create.php

---
+++ create.php
@@ -0,0 +1,93 @@
+<?php 
+	
+	require 'database.php';
+	if ( !empty($_POST)) {
+		// keep track validation errors
+		$emailError = null;
+		$passwordError = null;
+		
+		// keep track post values
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		
+		// validate input
+		$valid = true;
+		
+		if (empty($email)) {
+			$emailError = 'Please enter Email Address';
+			$valid = false;
+		} else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+			$emailError = 'Please enter a valid Email Address';
+			$valid = false;
+		}
+		
+		if (empty($password)) {
+			$passwordError = 'Please enter password';
+			$valid = false;
+		}
+		
+		// insert data
+		if ($valid) {
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO Users (email,password) values(?, ?)";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($email,$password));
+			Database::disconnect();
+			header("Location: index.php");
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+	<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'unsafe-inline'; style-src 'self' 'unsafe-inline'; media-src *" />
+				
+    <title>Project 7</title>
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Create a User</h3>
+		    		</div>
+    		
+	    			<form class="form-horizontal" action="create.php" method="post">
+					  <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+					    <label class="control-label">Email Address</label>
+					    <div class="controls">
+					      	<input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
+					      	<?php if (!empty($emailError)): ?>
+					      		<span class="help-inline"><?php echo $emailError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+					    <label class="control-label">Password</label>
+					    <div class="controls">
+					      	<input name="password" type="text"  placeholder="Password" value="<?php echo !empty($password)?$password:'';?>">
+					      	<?php if (!empty($passwordError)): ?>
+					      		<span class="help-inline"><?php echo $passwordError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-success">Create</button>
+						  <a class="btn" href="index.php">Back</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> 
+  </body>
+</html>
